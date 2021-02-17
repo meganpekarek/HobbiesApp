e7fd10980d597aaaccc9c3955bd536d1e988f22a
Megan Pekarek <mpekarek9@gmail.com>
2021-02-17 17:07:39 -0600

    home route setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './views/Home';
+import { Route, Switch, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/home" component={Home} />
+        </Switch>
+    </HashRouter>,
   document.getElementById('root')
 );
 
---
+++ src/views/Home.js
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+        <div>Home is working!</div>
+    );
+}
+
+export default Home;
